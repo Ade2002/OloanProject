188c9e652ca8bf9adde2c8c6e944a2c1b41174f4
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-07-17 14:46:30 +0100

    href removed

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.getElementById("openNav").onclick = function() {
    document.getElementById("openNav").style.display = "none"
-    document.getElementById("sideNav").setAttribute("style", 'width:250px; display:block;')
-
+   document.getElementById("sideNav").setAttribute("style", 'width:250px; display:block;')
+   
 }
 document.getElementById("closeNav").onclick = function() {
     document.getElementById("sideNav").setAttribute("style", 'width:0')
+    document.getElementById("openNav").style.display = "block"
 }
