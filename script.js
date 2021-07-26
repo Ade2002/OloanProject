7c485803d69c6ddb0672f6c86c4698c47f3c44e7
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-07-26 17:12:23 +0100

    Debugging started

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ document.getElementById("closeNav").onclick = function() {
     document.getElementById("sideNav").setAttribute("style", 'width:0')
     document.getElementById("openNav").style.display = "block"
 }
+
+
+let text = "Lorem Ipsum has been the industry's standard dummy....";
+let index = 0;
+
+function writeText() {
+    document.getElementById("lorem").innerHTML = text.slice(0, index);
+
+    index++;
+
+    if(index > text.length -0){
+        index = 0;
+    }
+}
+
+setInterval(writeText,150)
+
+
+
+
+
+
