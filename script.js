7919c215fe038462be1224de68744237a01ddf67
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-07-27 15:52:14 +0100

    Still Updating

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 let sideNav = document.getElementById("sideNav");
 let openNav =  document.getElementById("openNav");
-let closeNav = document.getElementById("closeNav")
-
-openNav.onclick = function() {
+let closeNav = document.getElementById("closeNav");
+const BREAKPOINT = 720;
+openNav.onclick = function(e) {
     openNav.style.display = "none";
     sideNav.style.display = "block";
-   
+    e.stopPropagation()
 }
-closeNav.onclick = function() {
+closeNav.onclick = function(e) {
+e.stopPropagation()
     sideNav.style.display = "none";
     openNav.style.display = "block";
 }
+window.onclick = function() {
+    if(BREAKPOINT){
+    sideNav.style.display = "none";
+    openNav.style.display = "block";
+}
+else{
+    openNav.style.display = "none";
+  }
+};
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let lorem = document.getElementById("lorem");
 let text = "Lorem Ipsum has been the industry's standard dummy....";
 let index = 0;
 
 function writeText() {
-    document.getElementById("lorem").innerHTML = text.slice(0, index);
+    lorem.innerHTML = text.slice(0, index);
 
     index++;
 
@@ -32,4 +59,16 @@ setInterval(writeText,150)
 
 
 
-
+let scrollTop = document.getElementById("scrollTop");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop >20 || document.documentElement.scrollTop > 20) {
+        scrollTop.style.display = "block";
+    } else {
+        scrollTop.style.display = "none";
+    }
+    scrollTop.onclick = function() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+  }
